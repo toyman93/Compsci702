706bb39b3a83e4d3d4d3b33a409ca1287a6a55e9
Kyungmo Peter Kang <kkan059@aucklanduni.ac.nz>
2015-03-23 23:28:33 +1300

    Added all possible permissions in the AndroidManifest file (everything except for system app permissions). Implemented a method that returns list of all packages and activities that are installed on the device.

--- Compsci702Project/app/src/main/java/com/compsci702project/compsci702project/MainActivity.java
+++ Compsci702Project/app/src/main/java/com/compsci702project/compsci702project/MainActivity.java
@@ -1,14 +1,27 @@
 package com.compsci702project.compsci702project;
 
+import android.content.Context;
+import android.content.ContextWrapper;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PermissionGroupInfo;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.List;
+
 
 public class MainActivity extends ActionBarActivity {
+
+    Context cont;
+    private ContextWrapper _context = new ContextWrapper(cont);
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,9 +32,13 @@ public class MainActivity extends ActionBarActivity {
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
 
+                try {
+                    listAllActivities();
+                } catch(PackageManager.NameNotFoundException e){}
+
                 //exit this application when the button is clicked
-                finish();
-                System.exit(0);
+                //finish();
+                //System.exit(0);
 
             }
         });
@@ -50,4 +67,27 @@ public class MainActivity extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
+
+    //Method which shows an example of using package manager to get all installed packages on this device
+    //Potentially useful, but more importantly, look at this if you are not sure how to use package manager.
+    //Code is pretty self explanatory
+    public void listAllActivities() throws PackageManager.NameNotFoundException
+    {
+        List<PackageInfo> packages = getPackageManager().getInstalledPackages(0);
+        for(PackageInfo pack : packages)
+        {
+            ActivityInfo[] activityInfo = getPackageManager().getPackageInfo(pack.packageName, PackageManager.GET_ACTIVITIES).activities;
+            Log.i("1", pack.packageName + " has total " + ((activityInfo==null)?0:activityInfo.length) + " activities");
+            if(activityInfo!=null)
+            {
+                for(int i=0; i<activityInfo.length; i++)
+                {
+                    Log.i("2", pack.packageName + " ::: " + activityInfo[i].name);
+                }
+            }
+        }
+    }
+
+
 }
